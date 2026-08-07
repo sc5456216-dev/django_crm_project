c530fc0fc03a557ac5837fd98b35f64e758c53f2
sameer <sc5456216@gmail.com>
2026-08-07 09:00:19 +0545

    Fix apps.py conflict in notifications

--- notifications/apps.py
+++ notifications/apps.py
@@ -1,14 +1,8 @@
-from django.apps import AppConfig
+﻿from django.apps import AppConfig
 
-<<<<<<< HEAD
-
-class NotificationsConfig(AppConfig):
-    name = 'notifications'
-=======
 class NotificationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notifications'
 
     def ready(self):
         import notifications.signals
->>>>>>> samir
